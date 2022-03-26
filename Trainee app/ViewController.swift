de565ce8fcd7c544b5161c661f6808e32a9ad2ea
StasonLV <talemane@gmail.com>
2022-03-26 10:56:58 +0300

    added project files

---
+++ Trainee app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trainee app
+//
+//  Created by Stanislav Lezovsky on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
