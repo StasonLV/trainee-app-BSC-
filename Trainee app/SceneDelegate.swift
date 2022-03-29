a03188586aec8a0b4f90183ed60e8d7502d61fc5
StasonLV <talemane@gmail.com>
2022-03-29 18:58:27 +0300

    Устранение замечаний от 29.03.22
    Поправлены отступы, сделана структура с константами для текстов, ключей(вместо енама), тайтла таббара. Текствью привязана к низу вью, добавлена прокрутка. Исправлены методы, чтобы не перегружать вьюдидлоад.

--- Trainee app/SceneDelegate.swift
+++ Trainee app/SceneDelegate.swift
@@ -13,16 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window?.windowScene = windowScene
-        window?.makeKeyAndVisible()
-        let rootVC = ViewController()
+        let rootVC = NoteViewController()
         let navVC = UINavigationController(rootViewController: rootVC)
         navVC.navigationBar.barStyle = .black
         window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
