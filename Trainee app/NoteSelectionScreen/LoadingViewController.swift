758fee035e94888015d3322a8fb58c5df387944f
StasonLV <talemane@gmail.com>
2022-05-24 23:22:01 +0300

    Изменен URL в соответствии с ТЗ, добавлены поля в модели, ИмейджВью для ячейки, экстеншн для добавления изображения по ссылке

---
+++ Trainee app/NoteSelectionScreen/LoadingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IndicatorExtension.swift
+//  Trainee app
+//
+//  Created by Stanislav Lezovsky on 24.05.2022.
+//
+
+import Foundation
+import UIKit
+final class LoadingViewController: UIViewController {
+    var loadingActivityIndicator: UIActivityIndicatorView = {
+        let indic = UIActivityIndicatorView()
+        indic.style = .medium
+        indic.color = .white
+        indic.startAnimating()
+        indic.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
+        return indic
+    }()
+
+    var blurEffectView: UIVisualEffectView = {
+        let blur = UIBlurEffect(style: .dark)
+        let blurView = UIVisualEffectView(effect: blur)
+        blurView.alpha = 0.6
+        blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return blurView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        blurEffectView.frame = self.view.bounds
+        view.insertSubview(blurEffectView, at: 0)
+        loadingActivityIndicator.center = CGPoint(
+            x: view.bounds.midX,
+            y: view.bounds.midY
+        )
+        view.addSubview(loadingActivityIndicator)
+    }
+}
